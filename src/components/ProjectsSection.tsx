87b0241720c8e2dbd767d6615f21bcf8b468bd57
Vedanti  Deshmukh <91212697+VedantiDeshmukh@users.noreply.github.com>
2026-05-05 22:45:00 +0530

    Update ProjectsSection.tsx

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -14,7 +14,7 @@ import Quiz from "@/assets/Riddleroom.png";
 // 3. Set the `image` field in the project object:
 //    image: researchAgentImg,
 
-const categories = ["All Projects", "AI/ML", "Generative AI", "Agentic AI", "Blockchain", "Data Analysis", "Android Development", "Web Dev"];
+const categories = ["All Projects", "AI/ML", "Generative AI", "Agentic AI","Data Analysis", "Blockchain",  "Android Development", "Web Dev"];
 
 interface Project {
   title: string;
